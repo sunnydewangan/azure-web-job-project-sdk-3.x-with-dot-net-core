8505b449a66f40402376ce3d330c1348cd1d2680
Sunny Dewangan <sadewa@microsoft.com>
2019-05-25 14:09:18 +0530

    Initial Commit

---
+++ Azure WebJobs with SDK 3.x/Program.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Azure_WebJobs_with_SDK_3.x
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .UseEnvironment("Development")
+                                .ConfigureAppConfiguration((context, config) =>
+                                {
+                                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);      //appsettings.json --> Copy Always to True
+                                })
+                .ConfigureWebJobs(b =>
+                {
+                    b.AddAzureStorageCoreServices()
+                    .AddAzureStorage();     //This is to register Queue Trigger Function
+                })
+                 .ConfigureLogging((context, b) =>
+                 {
+                     b.SetMinimumLevel(LogLevel.Debug);
+                 })
+                .UseConsoleLifetime();
+
+            var host = builder.Build();
+
+            using (host)
+            {
+                await host.RunAsync();
+            }
+
+        }
+    }
+}
---
+++ Azure WebJobs with SDK 3.x/QueueTriggerFunction.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Azure_WebJobs_with_SDK_3.x
+{
+    public class QueueTriggerFunction
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+
+        //This is a Queue Trigger Function, which listens to the queue named "testqueue"
+        public void ProcessWorkItem([QueueTrigger("testqueue")] string message, ILogger logger)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
